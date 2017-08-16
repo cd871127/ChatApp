733013a58eed1b8d5bbcb2de47eda2b8ddca14bb
cdistc <cdistc@163.com>
2017-08-16 17:07:44 +0800

    refactor

--- server/src/main/java/anthony/libs/chatapp/server/processor/ServerTextMessageProcessor.java
+++ server/src/main/java/anthony/libs/chatapp/server/processor/ServerTextMessageProcessor.java
@@ -1,15 +1,24 @@
 package anthony.libs.chatapp.server.processor;
 
+import anthony.libs.chatapp.core.message.MessageUtil;
 import anthony.libs.chatapp.core.message.TextMessage;
 import anthony.libs.chatapp.core.processor.AbstractTextMessageProcessor;
+import anthony.libs.chatapp.core.user.ClientInfo;
+import anthony.libs.chatapp.server.container.ClientInfoContainer;
+
+import java.nio.channels.SocketChannel;
 
 /**
  * Created by chend on 2017/8/14.
  */
 public class ServerTextMessageProcessor extends AbstractTextMessageProcessor {
+    private ClientInfoContainer clientInfoContainer = ClientInfoContainer.getInstance();
 
     @Override
     protected void doProcess(TextMessage message) {
-        System.out.println("TextMessageProcessor");
+        ClientInfo clientInfo = clientInfoContainer.get(message.getDestination());
+        if (clientInfo != null) {
+            MessageUtil.sendMessage(message, (SocketChannel) clientInfo.getSelectionKey().channel());
+        }
     }
 }
