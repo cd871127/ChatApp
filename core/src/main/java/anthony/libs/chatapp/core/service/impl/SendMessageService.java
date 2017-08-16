8b1e8fedcf4001f63df2e72940222958c8dfce17
cdistc <cdistc@163.com>
2017-08-16 17:59:18 +0800

    auto commit

---
+++ core/src/main/java/anthony/libs/chatapp/core/service/impl/SendMessageService.java
@@ -0,0 +1,14 @@
+package anthony.libs.chatapp.core.service.impl;
+
+import anthony.libs.chatapp.core.service.AbstractService;
+
+/**
+ * Created by chend on 2017/8/16.
+ * 发送消息的服务
+ */
+public class SendMessageService extends AbstractService {
+    @Override
+    protected void execute() {
+
+    }
+}
