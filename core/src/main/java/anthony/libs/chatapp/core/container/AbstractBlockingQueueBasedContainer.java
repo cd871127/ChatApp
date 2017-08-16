ba9f823a0d0ffe38df3b7cf21f26cc9e38c4bc24
cdistc <cdistc@163.com>
2017-08-16 18:13:08 +0800

    refactor

--- core/src/main/java/anthony/libs/chatapp/core/container/AbstractBlockingQueueBasedContainer.java
+++ core/src/main/java/anthony/libs/chatapp/core/container/AbstractBlockingQueueBasedContainer.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by chend on 2017/8/14.
@@ -22,5 +23,35 @@ public class AbstractBlockingQueueBasedContainer<V> {
         return container;
     }
 
-    protected 
+    public boolean add(V e) {
+        return container.add(e);
+    }
+
+    public boolean offer(V e) {
+        return container.offer(e);
+    }
+
+    public void put(V e) throws InterruptedException {
+        container.put(e);
+    }
+
+    public boolean offer(V e, long timeout, TimeUnit unit) throws InterruptedException {
+        return container.offer(e, timeout, unit);
+    }
+
+    public V take() throws InterruptedException {
+        return container.take();
+    }
+
+    public V poll(long timeout, TimeUnit unit) throws InterruptedException {
+        return container.poll(timeout, unit);
+    }
+
+    public boolean remove(Object o) {
+        return container.remove(o);
+    }
+
+    public boolean contains(Object o) {
+        return container.contains(o);
+    }
 }
